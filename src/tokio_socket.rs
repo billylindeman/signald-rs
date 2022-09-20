8d75ecd116018855e2c9e52e67de4c84ce1e4dfb
Billy Lindeman <billylindeman@gmail.com>
2022-09-20 12:11:56 -0400

    working subscriptions

--- src/tokio_socket.rs
+++ src/tokio_socket.rs
@@ -1,27 +1,28 @@
-use tokio::net::UnixStream;
-use tokio::net::unix::{OwnedReadHalf, OwnedWriteHalf};
-use tokio::sync::mpsc::{Sender, Receiver};
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
-use tokio::sync::mpsc;
-use tokio::io::BufReader;
-use std::sync::{Arc, Mutex};
-use std::collections::HashMap;
-use uuid::Uuid;
-use std::fmt::Debug;
 use async_trait::async_trait;
 use serde_json::Value;
+use std::collections::HashMap;
+use std::fmt::Debug;
 use std::io::Error;
 use std::path::Path;
+use std::sync::{Arc, Mutex};
+use tokio::io::BufReader;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
+use tokio::net::unix::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::net::UnixStream;
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::{Receiver, Sender};
+use uuid::Uuid;
 
+use crate::actions::SocketWrapper;
 use crate::errors::SignaldError;
 use crate::socket::AsyncSocket;
-use crate::actions::SocketWrapper;
+use crate::types::{ClientMessageWrapperV1, IncomingMessageV1};
 
 pub enum SocketError {
     General(&'static str),
     Io(Error),
     Channel(&'static str),
-    Signald(SignaldError)
+    Signald(SignaldError),
 }
 
 impl Debug for SocketError {
@@ -30,7 +31,7 @@ impl Debug for SocketError {
             SocketError::General(desc) => write!(f, "Error: {}", desc),
             SocketError::Io(e) => write!(f, "Error: {}", e),
             SocketError::Channel(e) => write!(f, "Error: {}", e),
-            SocketError::Signald(e) => write!(f, "Signald error: {}", e.error.message)
+            SocketError::Signald(e) => write!(f, "Signald error: {}", e.error.message),
         }
     }
 }
@@ -46,17 +47,19 @@ pub type Map = Arc<Mutex<HashMap<Uuid, (Sender<Value>, Option<Receiver<Value>>)>
 pub struct Socket<T> {
     socket: T,
     response_map: Map,
-    listening: Arc<Mutex<bool>>
+    listening: Arc<Mutex<bool>>,
+
+    pub subscriber: Receiver<IncomingMessageV1>,
 }
 
 #[async_trait]
 impl AsyncSocket for Socket<OwnedWriteHalf> {
     async fn write<'a>(&'a mut self, buf: &'a [u8], id: &Uuid) -> Result<(), SocketError> {
         let channel = mpsc::channel(1);
-        self.response_map.lock().unwrap().insert(
-            *id,
-            (channel.0, Some(channel.1))
-        );
+        self.response_map
+            .lock()
+            .unwrap()
+            .insert(*id, (channel.0, Some(channel.1)));
 
         self.socket.write_all(buf).await?;
         Ok(())
@@ -65,10 +68,15 @@ impl AsyncSocket for Socket<OwnedWriteHalf> {
     async fn get_response<'a>(&'a mut self, id: Uuid) -> Result<Value, SocketError> {
         let mut receiver = match self.response_map.lock().unwrap().get_mut(&id) {
             Some(channel) => channel.1.take().unwrap(),
-            None => { return Err(SocketError::General("Error: Incorrect response ID")); }
+            None => {
+                return Err(SocketError::General("Error: Incorrect response ID"));
+            }
         };
 
-        receiver.recv().await.ok_or(SocketError::Channel("Failed to receive response"))
+        receiver
+            .recv()
+            .await
+            .ok_or(SocketError::Channel("Failed to receive response"))
     }
 }
 
@@ -78,25 +86,29 @@ impl Socket<OwnedWriteHalf> {
         let response_map = Arc::new(Mutex::new(HashMap::new()));
         let listening = Arc::new(Mutex::new(true));
 
+        let (subscriber_tx, subscriber_rx) = mpsc::channel(32);
+
         let socket_wrapper = Socket {
             socket: writer,
             response_map: response_map.clone(),
-            listening: listening.clone()
+            listening: listening.clone(),
+            subscriber: subscriber_rx,
         };
 
         tokio::task::spawn(async move {
-            listen(
-                reader,
-                response_map,
-                listening
-            ).await;
+            listen(reader, response_map, listening, subscriber_tx).await;
         });
 
         Ok(socket_wrapper)
     }
 }
 
-async fn listen(socket: OwnedReadHalf, map: Map, listening: Arc<Mutex<bool>>) {
+async fn listen(
+    socket: OwnedReadHalf,
+    map: Map,
+    listening: Arc<Mutex<bool>>,
+    subscriber_tx: Sender<IncomingMessageV1>,
+) {
     let mut reader = BufReader::new(socket);
     let mut buf = String::with_capacity(1024);
 
@@ -105,18 +117,21 @@ async fn listen(socket: OwnedReadHalf, map: Map, listening: Arc<Mutex<bool>>) {
             Ok(_) => {
                 let response: Value = serde_json::from_str(buf.as_str()).unwrap();
 
-                println!("got response: {:?}", response);
                 if let Some(id) = response.get("id") {
                     let id = Uuid::parse_str(id.as_str().unwrap()).unwrap();
                     let sender = map.lock().unwrap().get(&id).unwrap().0.clone();
                     match sender.send(response).await {
                         Ok(_) => {
                             println!("sent response!");
-                        },
-                        Err(e) => println!("Error sending response: {}", e)
+                        }
+                        Err(e) => println!("Error sending response: {}", e),
                     }
+                } else if let Some("IncomingMessage") = response.get("type").unwrap().as_str() {
+                    let msg: IncomingMessageV1 =
+                        serde_json::from_value(response.get("data").unwrap().clone()).unwrap();
+                    subscriber_tx.send(msg).await.unwrap();
                 }
-            },
+            }
             Err(e) => {
                 println!("Error: {}", e.to_string());
             }
